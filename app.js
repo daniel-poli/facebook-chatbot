caafbb326729775bae7014471ef9c3c90829daf7
daniel-poli <108692201+daniel-poli@users.noreply.github.com>
2022-07-04 15:35:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -64,8 +64,8 @@ if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
  *
  */
 app.get('/webhook', function(req, res) {
-  console.log(req.query['hub.mode'];
-  console.log(req.query['hub.verify_token'];
+  console.log(req.query['hub.mode']);
+  console.log(req.query['hub.verify_token']);
   console.log('');
   console.log(VALIDATION_TOKEN);
   console.log(req.query['hub.verify_token'] === VALIDATION_TOKEN);
