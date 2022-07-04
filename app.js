8bec6d94cf70f0671116ec7b2adf2a0d1b629e02
daniel-poli <108692201+daniel-poli@users.noreply.github.com>
2022-07-04 15:29:28 -0300

    debug env variables

--- app.js
+++ app.js
@@ -54,6 +54,8 @@ const SERVER_URL = (process.env.SERVER_URL) ?
 if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
   console.error("Missing config values");
   process.exit(1);
+} else{
+  console.log(process.env);
 }
 
 /*
